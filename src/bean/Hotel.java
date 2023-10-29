80b9becd8fc66e4cfc53b2d92a236424ec9fd0c8
IsaMontoya17 <isabelamontoya2021@gmail.com>
2023-10-29 18:03:21 -0500

    primer commit

---
+++ src/bean/Hotel.java
@@ -0,0 +1,46 @@
+
+package bean;
+
+public class Hotel {
+    
+    private int idHotel;
+    private String nombre;
+    private String zona;
+    private int precio;
+
+    public Hotel() {
+    }
+
+    public int getIdHotel() {
+        return idHotel;
+    }
+
+    public void setIdHotel(int idHotel) {
+        this.idHotel = idHotel;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getZona() {
+        return zona;
+    }
+
+    public void setZona(String zona) {
+        this.zona = zona;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+    
+}//CIERRE DE LA CLASE
---
+++ src/vista/Inicio.java
@@ -0,0 +1,10 @@
+
+package vista;
+
+public class Inicio {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
